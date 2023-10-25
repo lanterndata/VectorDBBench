2db837264a33c5f240132fdbd1e8cce6b1408e34
Varik Matevosyan <varikmatevosyan@gmail.com>
2023-10-25 12:40:00 +0400

    Change sqlalchemy to psycopg

--- vectordb_bench/backend/clients/lantern/lantern.py
+++ vectordb_bench/backend/clients/lantern/lantern.py
@@ -5,24 +5,10 @@ import time
 import subprocess
 from contextlib import contextmanager
 from typing import Any, Tuple, Type
+import psycopg2
 
 from ..api import MetricType, VectorDB, DBConfig, DBCaseConfig, IndexType
 from .config import LanternConfig, LanternIndexConfig
-from sqlalchemy import (
-    create_engine,
-    insert,
-    Index,
-    Table,
-    text,
-    Column,
-    Integer,
-    ARRAY,
-    REAL
-)
-from sqlalchemy.orm import (
-    declarative_base, 
-    Session
-)
 
 log = logging.getLogger(__name__) 
 
@@ -48,26 +34,17 @@ class Lantern(VectorDB):
         self._drop_old = drop_old
 
         # construct basic units
-        pg_engine = create_engine(**self.db_config)
+        
+        pg_engine = psycopg2.connect(db_config['url'])
+        pg_engine.autocommit = True
+        pg_session = pg_engine.cursor()
         
         # create lantern extension
-        with pg_engine.connect() as conn: 
-            conn.execute(text('CREATE EXTENSION IF NOT EXISTS lantern'))
-            if drop_old:
-                log.info(f"Lantern client drop table : {self.table_name}")
-                conn.execute(text(f'DROP TABLE IF EXISTS "{self.table_name}" CASCADE'))
-            conn.commit()
-        
-        # redefined engine so metadata cache will be cleared after drop
-        pg_engine = create_engine(**self.db_config)
-        Base = declarative_base()
-        pq_metadata = Base.metadata
-        pq_metadata.reflect(pg_engine) 
-
-        self.pg_table = self._get_table_schema(pq_metadata)
-
-        if drop_old and self.table_name in pq_metadata.tables:
-            self._create_table(dim, pg_engine)
+        pg_session.execute('CREATE EXTENSION IF NOT EXISTS lantern')
+        if drop_old:
+            log.info(f"Lantern client drop table : {self.table_name}")
+            pg_session.execute(f'DROP TABLE IF EXISTS "{self.table_name}" CASCADE')
+            self._create_table(pg_session, dim)
 
     
     @classmethod
@@ -86,13 +63,9 @@ class Lantern(VectorDB):
             >>>     self.insert_embeddings()
             >>>     self.search_embedding()
         """
-        self.pg_engine = create_engine(**self.db_config)
-
-        Base = declarative_base()
-        pq_metadata = Base.metadata
-        pq_metadata.reflect(self.pg_engine) 
-        self.pg_session = Session(self.pg_engine)
-        self.pg_table = self._get_table_schema(pq_metadata)
+        self.pg_engine = psycopg2.connect(self.db_config['url'])
+        self.pg_engine.autocommit = True
+        self.pg_session = self.pg_engine.cursor()
         yield 
         self.pg_session = None
         self.pg_engine = None 
@@ -113,41 +86,26 @@ class Lantern(VectorDB):
     def ready_to_search(self):
         pass
 
-    def _get_table_schema(self, pq_metadata):
-        return Table(
-            self.table_name,
-            pq_metadata,
-            Column(self._primary_field, Integer, primary_key=True),
-            Column(self._vector_field, ARRAY(REAL)),
-            extend_existing=True
-        )
-    
-    def _create_index(self, pg_engine):
+    def _create_index(self, pg_session):
         index_param = self.case_config.index_param()
         if index_param['external']:
             return
 
-        index = Index(self._index_name, self.pg_table.c.embedding,
-            postgresql_using='hnsw',
-            postgresql_with={'m': index_param["m"], 'ef_construction': index_param['ef_construction'], 'ef': index_param['ef'], 'dim': self.dim},
-            postgresql_ops={'embedding': index_param["metric"]}
-        )
-        index.drop(pg_engine, checkfirst = True)
-        index.create(pg_engine)
+        pg_session.execute(f'DROP INDEX IF EXISTS "{self._index_name}"')
+        pg_session.execute(f'CREATE INDEX "{self._index_name}" ON "{self.table_name}" USING hnsw("{self._vector_field}") WITH (m={index_param["m"]}, ef_construction={index_param["ef_construction"]}, ef={index_param["ef"]}, dim={self.dim})')
 
-    def _create_table(self, dim, pg_engine : int):
+    def _create_table(self, pg_session, dim):
         try:
             # create table
-            self.pg_table.create(bind = pg_engine, checkfirst = True)
+            pg_session.execute(f'CREATE TABLE "{self.table_name}" ("{self._primary_field}" INT PRIMARY KEY, "{self._vector_field}" REAL[{dim}]);')
             # create vec index
-            self._create_index(pg_engine)
+            self._create_index(pg_session)
         except Exception as e:
             log.warning(f"Failed to create lantern table: {self.table_name} error: {e}")
             raise e from None
 
     def copy_embeddings_from_csv(self, csv_path):
-        self.pg_session.execute(text(f'COPY "{self.table_name}" ("{self._primary_field}", "{self._vector_field}") FROM \'{csv_path}\' WITH CSV'))
-        self.pg_session.commit()
+        self.pg_session.execute(f'COPY "{self.table_name}" ("{self._primary_field}", "{self._vector_field}") FROM \'{csv_path}\' WITH CSV')
         return None
 
     def insert_embeddings(
@@ -155,13 +113,12 @@ class Lantern(VectorDB):
         embeddings: list[list[float]],
         metadata: list[int],
         **kwargs: Any,
-    ) -> (int, Exception):
+    ) -> Tuple[int, Exception|None]:
         try:
             if self._drop_old:
                 return len(metadata), None
-            items = [dict(id = metadata[i], embedding=embeddings[i]) for i in range(len(metadata))]
-            self.pg_session.execute(insert(self.pg_table), items)
-            self.pg_session.commit()
+            items = [f"({i}, ARRAY{metadata[i]})" for i in range(len(metadata))]
+            self.pg_session.execute(f'INSERT INTO "{self.table_name}" ("{self._primary_field}", "{self._vector_field}") VALUES ({",".join(items)})')
             return len(metadata), None
         except Exception as e:
             log.warning(f"Failed to insert data into lantern table ({self.table_name}), error: {e}")   
@@ -174,10 +131,7 @@ class Lantern(VectorDB):
         return output.decode(), error.decode()
 
     def create_external_index(self):
-        if self.pg_table.columns.embedding.index:
-            log.info('Index already exists dropping...')
-            self.pg_session.execute(text(f'DROP INDEX {self._index_name} ON {self.table_name}'))
-            self.pg_session.commit()
+        self.pg_session.execute(f'DROP INDEX IF EXISTS {self._index_name}')
 
         if not self.external_index_dir.exists():
             log.info(f"external index file path not exist, creating it: {self.external_index_dir}")
@@ -209,8 +163,7 @@ class Lantern(VectorDB):
             raise Exception(err)
 
 
-        self.pg_session.execute(text(f'CREATE INDEX "{self._index_name}" ON "{self.table_name}" USING hnsw("{self._vector_field}") WITH (_experimental_index_path=\'{index_file_path}\')'))
-        self.pg_session.commit()
+        self.pg_session.execute(f'CREATE INDEX "{self._index_name}" ON "{self.table_name}" USING hnsw("{self._vector_field}") WITH (_experimental_index_path=\'{index_file_path}\')')
 
 
     def search_embedding(
@@ -220,17 +173,13 @@ class Lantern(VectorDB):
         filters: dict | None = None,
         timeout: int | None = None,
     ) -> Tuple[list[int], float]:
-        assert self.pg_table is not None
         vec_id = None
         filter_statement = ''
         if filters:
             vec_id = filters.get('id')
             filter_statement = f'WHERE "{self._primary_field}" > {vec_id}'
         
-        statement = text(f'SELECT "{self._primary_field}" FROM "{self.pg_table}" {filter_statement} ORDER BY "{self._vector_field}" <-> array{query} LIMIT {k}')
-        s = time.perf_counter()
-        res = self.pg_session.execute(statement).fetchall()
-        dur = time.perf_counter() - s
-        print(f'Query duration {dur}')
-        return [row[0] for row in res], dur
+        self.pg_session.execute(f'SELECT "{self._primary_field}" FROM "{self.table_name}" {filter_statement} ORDER BY "{self._vector_field}" <-> array{query} LIMIT {k}')
+        res = self.pg_session.fetchall()
+        return ([row[0] for row in res], 0)
         
